e8efa1f0a5bdaf69535e8bada3b1fe9fbffeec7f
Antoine <ant.delalande@gmail.com>
2024-03-20 11:21:49 +0100

    updated thread

---
+++ userbase.js
@@ -0,0 +1,34 @@
+
+const myUsers = [
+{
+    picture: "assets/img/cat-img2.jpg",
+    firstname: "Toto",
+    lastname: "Test",
+    messages: [
+        {
+            date: "le 10/02/2024",
+            message: `Coucou, c'est Tidus. Voilà mon premier message`
+        },
+        {
+            date: "le 13/02/2024",
+            message: `Coucou, c'est encore Tidus. Déjà un second message !.`
+        },
+    ]
+},
+
+{
+    picture: "assets/img/dog-7134183_1280.jpg",
+    firstname: "Tidus",
+    lastname: "Lambert",
+    messages: [
+        {
+            date: "le 10/02/2024",
+            message: `Coucou, c'est Tidus. Voilà mon premier message`
+        },
+        {
+            date: "le 13/02/2024",
+            message: `Coucou, c'est encore Tidus. Déjà un second message !.`
+        },
+    ]
+}
+]
