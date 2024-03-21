50b7437170fd3a23b306953a5117b615e9d0375d
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-03-21 14:24:10 +0100

    résponsive gallerie et petite correction

--- profil.js
+++ profil.js
@@ -1,18 +1,17 @@
 //rename profil
 
+const editProfile = document.querySelector(".btn");
+const newProfile = document.querySelector(".nameprofile");
+const newImage = document.querySelector(".imagedeprofile");
+const newBackground = document.querySelector(".imagedefond");
+const newCompteur = document.querySelector(".compteur");
 
-const editProfile = document.querySelector(".btn")
-const newProfile = document.querySelector(".nameprofile")
-const newImage = document.querySelector(".imagedeprofile")
-const newBackground = document.querySelector (".imagedefond")
+editProfile.addEventListener("click", function () {
+  newProfile.textContent = "Mickael Beaugrand";
+  newCompteur.textContent = "0 Abonnements 0 Abonnés  ";
+  newImage.src = "assets/img/cat-img2.jpg";
+  newBackground.style.backgroundImage =
+    'url("assets/img/42848087-lignes-de-code-numérique-bleu.jpg")';
+});
 
-editProfile.addEventListener("click", function () { 
-     newProfile.textContent  = "Mickael Beaugrand" ;
-      newImage.src = "assets/img/cat-img2.jpg";
-      newBackground.style.backgroundImage = 'url("assets/img/42848087-lignes-de-code-numérique-bleu.jpg")';
-    
-    }
-    
-) 
-
-
+//galery
