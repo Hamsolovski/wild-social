e66b8fe352a9db9868d934966e43ce7427e40ca0
Antoine <ant.delalande@gmail.com>
2024-03-21 16:58:21 +0100

    added dynamic form

--- userbase.js
+++ userbase.js
@@ -30,7 +30,7 @@ const myUsers = [
             },
             {
                 date: "13/02/2024",
-                message: `Qu'est-ce que je kiffe quand ils se mettent en cercle ! Je me mets au milieu, je fais trois tours sur moi-même, et je donne la balle et BIM, y en a un qui me la lance !`,
+                message: `Qu'est-ce que je kiffe quand ils se mettent en rond ! Je me mets au milieu, je fais trois tours sur moi-même, et je donne la balle et BIM, y en a un qui me la lance !`,
                 likes: 16
             },
             {
@@ -53,7 +53,7 @@ const myUsers = [
             },
             {
                 date: "14/02/2024",
-                message: `Quelqu'un veut se faire un Fortnite ?`,
+                message: `Quelqu'un veut faire un Fortnite ?`,
                 likes: 24
             },
 
@@ -77,6 +77,70 @@ const myUsers = [
                 likes: 24
             },
 
+
+        ]
+    },
+
+    {
+        picture: "assets/imgprofil/IMG_20240320_142152.jpg",
+        firstname: "Antoine",
+        lastname: "Delalande",
+        messages: [
+            {
+                date: "20/03/2024",
+                message: `Il est vraiment stylé ce réseau social ! Bravo les copains ! `,
+                likes: 2
+            },
+
+
+        ]
+    },
+
+    {
+        picture: "assets/imgprofil/IMG_20240320_142225.jpg",
+        firstname: "Manue",
+        lastname: "Curiant",
+        messages: [
+            {
+                date: "21/03/2024",
+                message: `What is Node ? 🎤🪩🕺`,
+                likes: 200
+            },
+
+
+        ]
+    },
+    {
+        picture: "assets/imgprofil/IMG_20240320_142138.jpg",
+        firstname: "Anthony",
+        lastname: "Dufrenot",
+        messages: [
+            {
+                date: "30/03/2024",
+                message: `Bonjour. C'est tout ce que j'avais à dire. Voilà.`,
+                likes: 1
+            },
+            {
+                date: "20/03/2024",
+                message: `Ce projet est magnifique. Je ne parle pas du tout sous la contrainte.`,
+                likes: 24
+            },
+
+
+        ]
+    },
+    {
+        picture: "assets/imgprofil/Sibadprofile.jpg",
+        firstname: "Adam",
+        lastname: "Maaloud",
+        messages: [
+            {
+                date: "30/03/2024",
+                message: `Hot take : Apple > Windows`,
+                likes: 200 
+            }
+
+            
 
         ]
     }
